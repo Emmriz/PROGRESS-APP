9288aae262c2f3c2acf8346af053188969e74113
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2025-09-09 12:00:01 +0100

    Generated the core pages. and also updated the route.ts file

--- learn-track/src/app/app.routes.ts
+++ learn-track/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SubmissionsComponent } from './pages/submissions/submissions.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'submissions', component: SubmissionsComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
